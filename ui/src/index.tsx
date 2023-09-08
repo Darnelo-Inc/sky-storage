99030cfe4aa592f79536d8409162903fe465d860
Pavel Gorbunov <darnelo@inbox.ru>
2023-09-08 16:06:50 +0300

    scss added

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -2,6 +2,7 @@ import ReactDOM from "react-dom/client"
 import App from "./App"
 import { Provider } from "react-redux"
 import { store } from "./store"
+import "./styles/index.scss"
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
