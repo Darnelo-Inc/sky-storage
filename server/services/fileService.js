1c16735862c07edd2f2d3286a65767f9768d32fc
Pavel Gorbunov <darnelo@inbox.ru>
2023-09-22 20:49:57 +0300

    FileService added

---
+++ server/services/fileService.js
@@ -0,0 +1,23 @@
+const fs = require("fs")
+const File = require("../models/File")
+
+class FileService {
+  createDir(file) {
+    const filePath = path.join(__dirname, `../files/${file.user}/${file.path}`)
+
+    return new Promise((resolve, reject) => {
+      try {
+        if (!fs.existsSync(filePath)) {
+          fs.mkdirSync(filePath)
+          return resolve({ message: "File was successfully created" })
+        } else {
+          return reject({ message: "File has already exists" })
+        }
+      } catch (error) {
+        return reject({ message: "Something went wrong" })
+      }
+    })
+  }
+}
+
+module.exports = new FileService()
