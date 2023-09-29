4646295fceefd02c5880c8ae7eba55a7af99c318
Pavel Gorbunov <darnelo@inbox.ru>
2023-09-29 11:54:21 +0300

    new font

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,6 +3,7 @@ import App from "./App"
 import { Provider } from "react-redux"
 import { store } from "./store"
 import "./styles/index.scss"
+import "./assets/fonts/PoetsenOne-Regular.ttf"
 import { BrowserRouter } from "react-router-dom"
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
