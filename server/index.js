e9dff66827151fb9fe60823eb2fd75e41f7ebada
Pavel Gorbunov <darnelo@inbox.ru>
2023-10-07 23:54:21 +0300

    https server implemented

--- server/index.js
+++ server/index.js
@@ -1,10 +1,16 @@
 require("dotenv").config()
+const https = require("https")
 const express = require("express")
 const fileUpload = require("express-fileupload")
 const mongoose = require("mongoose")
 const corsMiddleware = require("./middleware/cors.middleware")
 const authRouter = require("../server/routes/auth.routes")
 const fileRouter = require("../server/routes/file.routes")
+
+const options = {
+  cert: fs.readFileSync("/etc/nginx/sslInfo/cert.pem"),
+  key: fs.readFileSync("/etc/nginx/sslInfo/key.pem"),
+}
 
 const app = express()
 const PORT = process.env.serverPort
@@ -16,12 +22,14 @@ app.use(express.json())
 app.use("/api/auth", authRouter)
 app.use("/api/files", fileRouter)
 
+const server = https.createServer(options, app)
+
 async function start() {
   try {
     await mongoose.connect(dbURL)
 
-    app.listen(PORT, () => {
-      console.log(`Server started on port ${PORT}`)
+    server.listen(443, () => {
+      console.log("Server is running on port 443")
     })
   } catch (error) {
     console.log(error)
