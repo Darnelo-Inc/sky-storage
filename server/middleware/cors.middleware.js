d05594c2508af285583929a3a46025a1555116cb
Pavel Gorbunov <darnelo@inbox.ru>
2023-10-11 21:22:46 +0300

    rm js from server

--- server/middleware/cors.middleware.js
+++
@@ -1,8 +0,0 @@
-function cors(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH")
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization")
-  next()
-}
-
-module.exports = cors
