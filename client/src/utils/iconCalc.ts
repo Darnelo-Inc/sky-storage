d24e8949d5f1acf56f67454d821991a75f3f9583
Pavel Gorbunov <darnelo@inbox.ru>
2023-10-13 17:09:16 +0300

    img icon added

--- client/src/utils/iconCalc.ts
+++ client/src/utils/iconCalc.ts
@@ -2,6 +2,7 @@ import dir__icon from "../assets/images/icons/dir.svg"
 import music__icon from "../assets/images/icons/music.svg"
 import txt__icon from "../assets/images/icons/txt.svg"
 import video__icon from "../assets/images/icons/video.svg"
+import img__icon from "../assets/images/icons/img.svg"
 
 type IconMap = {
   [key in string]: string
@@ -12,5 +13,14 @@ export const ICONS: IconMap = {
   mp3: music__icon,
   mp4: video__icon,
   txt: txt__icon,
+  doc: txt__icon,
+  docx: txt__icon,
   pages: txt__icon,
+  pdf: txt__icon, // create pdf icon
+  jpg: img__icon,
+  jpeg: img__icon,
+  png: img__icon,
+  svg: img__icon,
+  // archive icon
+  // unknown icon
 }
