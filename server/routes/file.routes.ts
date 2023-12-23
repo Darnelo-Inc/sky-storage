4ff325e807d2b273a705047d2b6b2a4136c7b7c8
Pavel Gorbunov <darnelo@inbox.ru>
2023-12-23 12:10:01 +0300

    deduplication middleware added

--- server/routes/file.routes.ts
+++ server/routes/file.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express"
 import fileController from "../controllers/fileController"
 import authMiddleware from "../middleware/auth.middleware"
+import deduplicateMiddleware from "../middleware/deduplication.middleware"
 
 const router = Router()
 
@@ -8,7 +9,12 @@ router.get("", authMiddleware, fileController.getFiles)
 router.get("/download", authMiddleware, fileController.downloadFile)
 
 router.post("", authMiddleware, fileController.createDir)
-router.post("/upload", authMiddleware, fileController.uploadFile)
+router.post(
+  "/upload",
+  authMiddleware,
+  deduplicateMiddleware,
+  fileController.uploadFile
+)
 router.delete("/", authMiddleware, fileController.deleteFile)
 
 export default router
